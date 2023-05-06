6c85e8ab14ac1eec258fd9cb4ffd675be6e126a8
DT-NetOps <dariusz.terefenko@gmail.com>
2023-05-06 16:58:00 +0000

    commit rds.tf

---
+++ rds.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  max_allocated_storage = 100
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
